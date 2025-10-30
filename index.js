04faef3f30597e4a9b14bc03bfbcbf2bda07fd72
Hingamakena <132439601+Hingamakena@users.noreply.github.com>
2025-10-30 10:45:19 +0300

    index js file to house the data

---
+++ index.js
@@ -0,0 +1,11 @@
+const livingroom = [
+  {"image_owner_reference": "https://dlifeinteriors.com/",
+   "image_source": "https://cdn-dliin.nitrocdn.com/iFaxlHcVqyWZWghwsjNiQOatlfNsVALG/assets/images/source/rev-b13ef66/dlifeinteriors.com/wp-content/uploads/2019/11/Home-interiors-in-kochi-kerala-1024x683.jpg"
+  },
+  {},
+  {},
+  {},
+  {},
+  {},
+  {}
+]
